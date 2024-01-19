87b8d112f285afa3411a05ada778828ec2ca90c3
emilahnberg123 <emil.ahnberg@elev.ga.ntig.se>
2024-01-19 12:45:40 +0100

    Add new pages

--- Elev/Startsida/kurser/todo.php
+++ Elev/Startsida/kurser/todo.php
@@ -1,6 +1,7 @@
-<?php 
-require "connect.php"
+<?php
+require "../connect.php";
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,12 +10,13 @@ require "connect.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Softroom Elev</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="todo.css">
 </head>
 <body>
     
 <div id="title">
     <h1>Softroom</h1>
+    
 </div>
 
 
@@ -56,36 +58,48 @@ require "connect.php"
  </div>
 </div>
 
+<div class="box">
+ 
+<h1 class="rubrik"> LÄMNA IN</h1>
 
+    <div class="göra">
 
-<div id="news">
-    <h1>Nyheter</h1>
-    <div class="image-container">
-        <div class="image-item">
-            <img>
-            <p>Skolmaten</p>
-        </div>
+      <h3 class="rubrik"> Engelska 7 </h3>
 
-        <div class="image-item">
-            <img>
-            <p>Jag om 1 månad</p>
-        </div>
+      <p class="info"> Lämna in uppgiften senast 2024-07-23</p>
 
-        <div class="image-item">
-            <img>
-            <p>Jag har inte duschat sen igår och jag har inte gillat mig själv sen 2006</p>
-        </div>
     </div>
+
+    <div class="göra">
+
+      <h3 class="rubrik"> Ämne </h3>
+
+      <p class="info"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto ullam optio perspiciatis facilis dolor inventore assumenda dolorum, unde vero harum laboriosam ducimus, itaque blanditiis eos minus. Praesentium exercitationem possimus pariatur! </p>
+
+    </div>
+
+    <div class="göra">
+
+      <h3 class="rubrik"> Ämne </h3>
+
+      <p class="info"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto ullam optio perspiciatis facilis dolor inventore assumenda dolorum, unde vero harum laboriosam ducimus, itaque blanditiis eos minus. Praesentium exercitationem possimus pariatur! </p>
+
+    </div>
+
+    <div class="göra">
+
+      <h3 class="rubrik"> Ämne </h3>
+
+      <p class="info"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto ullam optio perspiciatis facilis dolor inventore assumenda dolorum, unde vero harum laboriosam ducimus, itaque blanditiis eos minus. Praesentium exercitationem possimus pariatur! </p>
+
+    </div>
+
+
 </div>
 
-<hr>
 
-<div class="schedule-container">
-<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FStockholm&src=am9uYXRhbi5wcmFzdEBlbGV2LmdhLm50aWcuc2U&src=Y19jbGFzc3Jvb200NzEyOGEwYUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Z2EubnRpZy5zZV9jbGFzc3Jvb21lYjVkODU1NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb21lYjI0OWQ3YkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb203NDhiNDUyZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb200YjU4NDhkNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb201N2RkYjg1MUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb204MTg5ZTlkYUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb21jMzZkODk0YUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb20wYjEyNDIyN0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb20zYzZiYWU4YkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Z2EubnRpZy5zZV9jbGFzc3Jvb204MzJmOWViZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb204ZjgzYzA0NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb21lZTYyN2QwOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=c3Yuc3dlZGlzaCNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Z2EubnRpZy5zZV9jbGFzc3Jvb21jY2U5YmQ0OUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb203Mzg2ZmFiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Z2EubnRpZy5zZV9jbGFzc3Jvb20xZjhmYjkxN0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Z2EubnRpZy5zZV9jbGFzc3Jvb20zZTY5MzU0N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Z2EubnRpZy5zZV9jbGFzc3Jvb21hNjM1YWU2NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb20zOWIzNTIxYUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb200YzMwOWYyNUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb205ZDIzOTkwZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23137333&color=%23202124&color=%23c26401&color=%23137333&color=%23137333&color=%230047a8&color=%230047a8&color=%230047a8&color=%23202124&color=%23202124&color=%230047a8&color=%230047a8&color=%237627bb&color=%230B8043&color=%23007b83&color=%230047a8&color=%230047a8&color=%23c26401&color=%23c26401&color=%23202124&color=%23174ea6&color=%230047a8" 
-style=""width="800" height="600" frameborder="0" scrolling="no"></iframe>
-</div>
 
-<hr>
+
 
 <div class="kurser">
         <?php
@@ -130,11 +144,11 @@ style=""width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
 
 <footer>
-    <h2>&copy; 2023 Softroom</h2>
+    <h2>&copy; 2024 Softroom</h2>
 </footer>
 
 
-<script src="script.js"></script>
+<script src="../script.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
 </body>
